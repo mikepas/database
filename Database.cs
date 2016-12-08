0558c9f1b521afc22c1628551d4f29ff926c3885
Mike Pas <mike.pas@hotmail.com>
2016-12-08 10:03:34 +0100

    Update Database.cs

--- Database.cs
+++ Database.cs
@@ -1,4 +1,3 @@
-﻿using System;
 using System.Data;
 using System.Windows.Forms;
 using MySql.Data.MySqlClient;
@@ -7,51 +6,38 @@ namespace DatabaseClass
 {
     class Database
     {
-        private MySqlConnection _conn;
 
-        private void Connect(string server, string uid, string pwd, string database)
+        public static bool Login(string server, string uid, string pwd, string database, string query)
         {
-            try
-            {
-                string connStr = string.Format("server={0};uid={1};pwd={2};database={3}", server, uid, pwd, database);
-                _conn = new MySqlConnection(connStr);
-                _conn.Open();
-            }
-            catch (Exception)
-            {
-                // ignored
-            }
-        }
-
-        public bool Login(string server, string uid, string pwd, string database, string query)
-        {
-            Connect(server, uid, pwd, database);
-
             bool result = false;
 
-            if (_conn.State == ConnectionState.Open)
+            try
             {
-                MySqlCommand mscCommand = new MySqlCommand(query, _conn);
-                MySqlDataReader queryresult = mscCommand.ExecuteReader();
-                result = queryresult.Read();
-                _conn.Close();
+                MySqlConnection conn = new MySqlConnection(string.Format("server={0};uid={1};pwd={2};database={3}", server, uid, pwd, database));
+                conn.Open();
+
+                if (conn.State == ConnectionState.Open)
+                {
+                    MySqlCommand mscCommand = new MySqlCommand(query, conn);
+                    MySqlDataReader queryresult = mscCommand.ExecuteReader();
+                    result = queryresult.Read();
+                    conn.Close();
+                }
             }
-            else
+            catch
             {
                 MessageBox.Show(@"Could not connect to the database");
                 return false;
             }
 
-            if (result)
-            {
-                MessageBox.Show(@"You are logged in");
-                return true;
-            }
-            else
+            if (!result)
             {
                 MessageBox.Show(@"Incorrect username or password");
                 return false;
             }
+
+            MessageBox.Show(@"You are logged in");
+            return true;
         }
     }
 }
